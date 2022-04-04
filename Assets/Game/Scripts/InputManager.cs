12e3b89e3de845e7d77f93e6a148d90863f2a6a5
OmerAsik <faruk.asikoglu@gmail.com>
2022-04-04 16:50:56 +0300

    Initial commit

---
+++ Assets/Game/Scripts/InputManager.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine.InputSystem;
+
+[DefaultExecutionOrder(-1)]
+public class InputManager : Singleton<InputManager>
+{
+    //public event Action<Vector2> OnTouchActionPerformed;
+
+    public event Action<Vector2> OnTouchPerformed;
+    public event Action OnTouchEnded;
+    public event Action<Vector2> OnSwervePerformed; //swerve amount
+
+    private TouchControlMap touchControlMap;
+
+    private void Awake()
+    {
+        touchControlMap = new TouchControlMap();
+    }
+
+    private void OnEnable() => touchControlMap.Enable();
+    private void OnDisable() => touchControlMap.Disable();
+    private void Start()
+    {
+        Observer();
+    }
+
+    private void Observer()
+    {
+        //touchControlMap.Touch.PrimaryTouchContact.started += ctx => IsTouching(ctx);
+        //touchControlMap.Touch.Touch.canceled += EndTouch;
+
+        touchControlMap.Touch.TouchPos.performed += PerformTouch;
+        touchControlMap.Touch.Swerve.performed += PerformSwerve;
+
+        touchControlMap.Touch.IsTouching.canceled += EndTouch;
+
+    }
+
+    private void PerformSwerve(InputAction.CallbackContext context)
+    {
+        if (IsPointerOutsideTheBorder()) return;
+
+        OnSwervePerformed?.Invoke(touchControlMap.Touch.Swerve.ReadValue<Vector2>());
+    }
+
+    private void PerformTouch(InputAction.CallbackContext context)
+    {
+        if (IsPointerOutsideTheBorder()) return;
+
+        OnTouchPerformed?.Invoke(touchControlMap.Touch.TouchPos.ReadValue<Vector2>());
+    }
+
+    private void EndTouch(InputAction.CallbackContext context)
+    {
+        if (IsPointerOutsideTheBorder()) return;
+
+        OnTouchEnded?.Invoke();
+    }
+    private bool IsPointerOutsideTheBorder()
+    {
+        return float.IsInfinity(touchControlMap.Touch.TouchPos.ReadValue<Vector2>().x);
+    }
+}
+
+//private void PerformTouch(InputAction.CallbackContext context)
+//{
+//    if (float.IsPositiveInfinity(touchControlMap.Touch.Touch.ReadValue<Vector2>().x) ||
+//        float.IsNegativeInfinity(touchControlMap.Touch.Touch.ReadValue<Vector2>().x)) return;
+//    OnTouchActionPerformed?.Invoke(touchControlMap.Touch.Touch.ReadValue<Vector2>());
+//}
+/*, (float)context.startTime*/
---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using DG.Tweening;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, Foldout("[Input]")] private InputManager inputManager;
+    [SerializeField, Foldout("[Input]")] private bool isVerticalSwerveActive, isHorizontalSwerveActive;
+
+    private Vector3 goCoord, worldOffsetPos;
+    private bool isTouchingObject;
+    public Camera cam;
+
+    private void Awake()
+    {
+        Observer();
+    }
+
+    private void Observer()
+    {
+        inputManager.OnSwervePerformed += OnSwervePerformed;
+        inputManager.OnTouchPerformed += OnTouchPerformed;
+        inputManager.OnTouchEnded += OnTouchEnded;
+    }
+
+    private void OnTouchEnded()
+    {
+        isTouchingObject = false;
+    }
+
+    private void OnTouchPerformed(Vector2 coord)
+    {
+        Ray ray = UtilsClass.GetScreenPointToRay(coord);
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, 150f)) return;
+        if (hit.collider.CompareTag(" "))
+            isTouchingObject = true;
+    }
+
+
+    private void OnSwervePerformed(Vector2 swerveOffset)
+    {
+        if (!isTouchingObject) return;
+        //coord:ekranda, pos:dünyada, offsette z deðiþmez
+
+        Vector3 goWorldPosition = gameObject.transform.position;
+
+        goCoord = UtilsClass.GetWorldToScreenPoint(goWorldPosition); //Z'si 10 ekranda 
+        worldOffsetPos = UtilsClass.GetScreenToWorldPoint(goCoord + (Vector3)swerveOffset);
+
+        if (isVerticalSwerveActive) VerticalSwerve();
+        if (isHorizontalSwerveActive) HorizontalSwerve();
+    }
+
+    private void VerticalSwerve()
+    {
+        transform.position = new Vector3(transform.position.x, worldOffsetPos.y, transform.position.z);
+    }
+    private void HorizontalSwerve()
+    {
+        transform.position = new Vector3(worldOffsetPos.x, transform.position.y, transform.position.z);
+    }
+}
