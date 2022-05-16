f707228ba73d29f811a942c494316eb3459f0f03
OmerAsik <faruk.asikoglu@gmail.com>
2022-05-16 15:16:47 +0300

    Initialize LaserTest

--- Assets/Game/Scripts/CameraHandler.cs
+++ Assets/Game/Scripts/CameraHandler.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Cinemachine;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 
@@ -18,7 +21,7 @@ public class CameraHandler : MonoBehaviour {
 
     private CinemachineVirtualCamera currentCam;
     private Cam? oldCam; //bcs while first time OpenCam() returns 0
-
+    private TimerClass timer;
     private void OnEnable() => GameManager.OnStateChanged += GameManager_OnStateChanged;
     private void OnDisable() => GameManager.OnStateChanged -= GameManager_OnStateChanged;
     private void GameManager_OnStateChanged(GameState gameState) {
@@ -28,15 +31,28 @@ public class CameraHandler : MonoBehaviour {
             case GameState.Run: OpenCam(Cam.Running); break;
             case GameState.Win:
                 OpenCam(Cam.FinalPose);
-                StartCoroutine(UtilsClass.Wait(() =>
+                timer ??= new TimerClass
                 {
-                    OpenCam(Cam.FinalPose2);
-                }, 0f)); break;
+                    timer = 3f
+                };
+                //StartCoroutine(UtilsClass.Wait(() =>
+                //{
+                //    OpenCam(Cam.FinalPose2);
+                //}, 0f)); 
+                break;
             case GameState.Lose: OpenCam(Cam.FinalPose); break;
             default: break;
         }
     }
 
+    private void Update() {
+        if (timer != null) timer.timer -= Time.deltaTime;
+        if (timer?.timer <= 0f) OpenCam(Cam.FinalPose2);
+    }
+
+    private class TimerClass {
+        public float timer;
+    }
     private void OpenCam(Cam newCam) {
         if (oldCam == newCam) return;
 
@@ -50,6 +66,28 @@ public class CameraHandler : MonoBehaviour {
     private void ActivateNewCam(CinemachineVirtualCamera newCam) {
         foreach (CinemachineVirtualCamera cam in vCamList) {
             cam.gameObject.SetActive(newCam == cam);
+        }
+    }
+}
+
+namespace Test {
+    class LINQTest : MonoBehaviour {
+        enum BoolNames {
+            asd = 0,
+        }
+
+        private BoolNames boolNames;
+        private Dictionary<BoolNames, bool> newDic;
+
+        private void Awake() {
+
+            newDic = new Dictionary<BoolNames, bool>();
+        }
+
+        void St() {
+
+            int totalTrue = newDic.Where
+                ((t, i) => newDic[(BoolNames)i].Equals(true)).Count();
         }
     }
 }
